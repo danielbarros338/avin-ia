c0249a8d11f3c73d4757e781e1429fedea84239f
Daniel <usrrufus12@omint.com.br>
2023-12-05 16:48:23 -0300

    Training created

--- model/ai.py
+++ model/ai.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import numpy as np
 
 from tensorflow import keras
 # from tensorflow.keras import layers
@@ -8,13 +9,15 @@ from tensorflow import keras
 class Ai():
   def __init__(self, data):
     self.data = self.__formatData__(data)
-    # self.model = self.build_model()
+    self.model = self.build_model()
 
   def __formatData__(self, data):
     dfData = []
 
     for df in data['timeSeriesDailyArr']:
       self.__normalize_data__(df, dfData)
+
+    self.labels = dfData[0].index
 
     return dfData
   
@@ -33,44 +36,65 @@ class Ai():
     dfDataTemp[dfDataTemp.columns] = dfDataTemp[dfDataTemp.columns].apply(pd.to_numeric, errors='coerce')
     dfDataTemp[dfDataTemp.columns] = tf.keras.utils.normalize(dfDataTemp[dfDataTemp.columns].values, axis=0)
 
-    # Retirando o ano dos indices
-    dictIndex = {}
-    for i in range(0, len(dfDataTemp.index)):
-      dictIndex[dfDataTemp.index[i]] = dfDataTemp.index[i][5:]
+    # # Retirando o ano dos indices
+    # dictIndex = {}
+    # for i in range(0, len(dfDataTemp.index)):
+    #   dictIndex[dfDataTemp.index[i]] = dfDataTemp.index[i][5:]
 
-    dfDataTemp.rename(index=dictIndex, inplace=True)
+    # dfDataTemp.rename(index=dictIndex, inplace=True)
     dfDataTemp.index.name = 'Dates'
 
     arr.append(dfDataTemp)
-
   
   def training(self):
-    openValues = self.data[0]['1. open']
-    split_index = int(0.8 * len(openValues))
-    train_data, test_data = openValues.iloc[:split_index], openValues[split_index:]
+    highValues = self.data[0]['2. high'].values
+    lowValues = self.data[0]['3. low'].values
 
-    print(openValues.columns)
-    print(train_data.shape, test_data.shape)
+    split_index = int(0.8 * len(highValues))
+    train_data, test_data = highValues[:split_index], highValues[split_index:]
+
+    X_train, y_train = np.array(train_data), np.array(lowValues[:split_index])
+    X_test, y_test = np.array(test_data), np.array(lowValues[split_index:])
+
+    print(X_train, y_train)
+
+    self.history = self.model.fit(
+      x=X_train,
+      y=y_train,
+      epochs=20,
+      verbose=True
+    )
+
+    predictions_normalized = self.model.predict(X_test)
+
+    # Calcular MAE (Erro Absoluto Médio)
+    mae_tf = tf.reduce_mean(tf.abs(y_test - predictions_normalized)).numpy()
+
+    # Calcular MSE (Erro Quadrático Médio)
+    mse_tf = tf.reduce_mean(tf.square(y_test - predictions_normalized)).numpy()
+
+    # Calcular RMSE (Raiz Quadrada do Erro Quadrático Médio)
+    rmse_tf = tf.sqrt(mse_tf)
+
+    print(f"MAE (TensorFlow): {mae_tf}")
+    print(f"MSE (TensorFlow): {mse_tf}")
+    print(f"RMSE (TensorFlow): {rmse_tf}")
 
   def build_model(self):
     model = keras.Sequential([
-      keras.layers.Input(shape=(5,)),
-      keras.layers.Dense(64, activation='relu'),
-      keras.layers.Dense(32, activation='relu'),
-      keras.layers.Dense(5, activation='linear')
+      keras.layers.Dense(65, activation='relu', input_shape=[1]),
+      keras.layers.Dense(37, activation='relu'),
+      keras.layers.Dense(1, activation='linear')
     ])
 
-    optimizer = tf.keras.optimizers.RMSprop(0.001)
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
 
-    model.compile(loss='mse', optimizer=optimizer, metrics=['accuracy'])
+    model.compile(loss='mae', optimizer=optimizer, metrics=['mae', 'mse'])
 
     return model
   
   def predict(self):
     self.training()
-    # example_batch = self.training_test['train_dataset'][:10]
-    # example_result = self.model.predict(example_batch)
-    # print('result', example_result)
 
   def plot(self):
     # hist = pd.DataFrame(self.history.history)
@@ -82,20 +106,8 @@ class Ai():
     plt.figure()
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
-    plt.plot(hist['loss'], hist['epoch'],
-            label='Loss')
-    plt.plot(hist['accuracy'], hist['epoch'],
-            label = 'Accuracy')
-    plt.ylim([0,5])
-    plt.legend()
-
-    plt.figure()
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss')
-    plt.plot(hist['loss'], hist['epoch'],
-            label='Loss')
-    plt.plot(hist['accuracy'], hist['epoch'],
-            label = 'Accuracy')
-    plt.ylim([0,20])
+    plt.plot(hist['epoch'], hist['loss'], label='Loss')
+    plt.plot(hist['epoch'], hist['mse'], label = 'MSE')
+    plt.plot(hist['epoch'], hist['mae'], label = 'MAE')
     plt.legend()
     plt.show()
